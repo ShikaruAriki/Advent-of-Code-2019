45b41f1f1f46a8a3b65787f17dbb50a402e8290a
Stefano Ravetta <s.ravetta94@gmail.com>
2019-12-14 14:32:16 +0100

    [Day 4]

--- run_advent_of_code_2019.py
+++ run_advent_of_code_2019.py
@@ -1,6 +1,7 @@
 from day_1 import get_total_fuel, get_total_fuel_correct
 from day_2 import get_first_code_of_restored_gravity_assist_test_program, get_input_of_restored_gravity_assist_program
 from day_3 import get_manhattan_distance_from_central_point, get_minimized_signal_delay_from_central_point
+from day_4 import passwords_meet_criteria, passwords_meet_criteria_2
 
 # -- Day 1 -------------------------------------------------------------------------------------------------------------
 
@@ -23,3 +24,10 @@ print "The input of restored program is: " + str(get_input_of_restored_gravity_a
 print "The Manhattan distance is: " + str(get_manhattan_distance_from_central_point()) + "\n"
 
 print "The minimized signal delay is: " + str(get_minimized_signal_delay_from_central_point()) + "\n"
+
+# -- Day 4 -------------------------------------------------------------------------------------------------------------
+
+print "The numbers of passwords that meet the criteria are: " + str(passwords_meet_criteria(236491, 713787)) + "\n"
+
+print "The numbers of passwords that meet the second criteria are: " + str(passwords_meet_criteria_2(236491, 713787)) \
+      + "\n"
